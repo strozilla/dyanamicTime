864d00bb6db9bee182eb755fed0d37e0ef806b83
Castro <74827509+strozilla@users.noreply.github.com>
2021-07-11 08:36:08 -0500

    edit to files /styles, /App and /index

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  let time = new Date().toLocaleTimeString();
+
+  setInterval(getTime, 1000);
+
+  const [now, currTime] = useState(time);
+
+  function getTime() {
+    const upTime = new Date().toLocaleTimeString();
+    currTime(upTime);
+  }
+
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{now}</h1>
+      <button onClick={getTime}>Get Time</button>
     </div>
   );
 }
